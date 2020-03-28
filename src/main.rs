2561ba716222a57c4fd7a8a38e8b6b6c75a5c685
Sarp Basaraner <sarpbasaraner@gmail.com>
2020-03-28 15:42:24 +0300

    impl naive main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        2 => runFile(args[1].to_string()),
+        1 => runPrompt(),
+        _ => {
+            println!("Usage: rlox [script]");
+            std::process::exit(64);
+        },
+    }
+}
+
+fn runPrompt() {
+    unimplemented!();
+}
+
+fn runFile(file: String) {
+    unimplemented!();
 }
