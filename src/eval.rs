2f59b082202d2aec24ab70eb9c7d8a34399eb422
Sarp Basaraner <sarpbasaraner@gmail.com>
2020-04-05 08:09:36 +0300

    refactor

--- src/eval.rs
+++ src/eval.rs
@@ -1,5 +1,5 @@
 use crate::grammar::Expr;
-use crate::token::{Literal, TokenType};
+use crate::token::{Literal, TokenType, Token};
 use crate::RloxError;
 use std::fmt;
 
@@ -10,38 +10,42 @@ pub trait Evaluable {
 impl Evaluable for Expr {
     fn evaluate(&self) -> Result<Value, RloxError> {
         match self {
-            Expr::Binary { left, operator, right } => {
-                left.evaluate().and_then(|left| right.evaluate().and_then(|right| {
-                    match operator.details().token_type {
-                        TokenType::Greater => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Boolean(l > r))),
-                        TokenType::GreaterEqual => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Boolean(l >= r))),
-                        TokenType::Less => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Boolean(l < r))),
-                        TokenType::LessEqual => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Boolean(l <= r))),
-                        TokenType::Minus => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Number(l - r))),
-                        TokenType::Slash => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Number(l / r))),
-                        TokenType::Star => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Number(l * r))),
-                        TokenType::Plus => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Number(l + r)))
-                            .or_else(|_| cast_strs(&left, &right).and_then(|(l, r)| Ok(Value::String(format!("{}{}", l, r)))))
-                            .or(Err(cast_err("string or number"))),
-                        TokenType::EqualEqual => Ok(Value::Boolean(left.is_equal(&right))),
-                        TokenType::BangEqual => Ok(Value::Boolean(!left.is_equal(&right))),
-                        _ => Ok(Value::Nil) // unreachable
-                    }
-                }))
-            },
+            Expr::Binary { left, operator, right } => eval_binary(left, operator, right),
             Expr::Grouping(expr) => expr.evaluate(),
             Expr::Literal(literal) => Ok(Value::from(literal)),
-            Expr::Unary { operator, right } => {
-                right.evaluate().and_then(|right| {
-                    match operator.details().token_type {
-                        TokenType::Bang => Ok(Value::Boolean(!right.is_truthy())),
-                        TokenType::Minus => right.cast_number().and_then(|n| Ok(Value::Number(-n))),
-                        _ => Ok(Value::Nil) // unreachable
-                    }
-                })
-            }
+            Expr::Unary { operator, right } => eval_unary(operator, right)
         }
     }
+}
+
+fn eval_unary(operator: &Token, right: &Box<Expr>) -> Result<Value, RloxError> {
+    right.evaluate().and_then(|right| {
+        match operator.details().token_type {
+            TokenType::Bang => Ok(Value::Boolean(!right.is_truthy())),
+            TokenType::Minus => right.cast_number().and_then(|n| Ok(Value::Number(-n))),
+            _ => Ok(Value::Nil) // unreachable
+        }
+    })
+}
+
+fn eval_binary(left: &Box<Expr>, operator: &Token, right: &Box<Expr>) -> Result<Value, RloxError> {
+    left.evaluate().and_then(|left| right.evaluate().and_then(|right| {
+        match operator.details().token_type {
+            TokenType::Greater => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Boolean(l > r))),
+            TokenType::GreaterEqual => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Boolean(l >= r))),
+            TokenType::Less => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Boolean(l < r))),
+            TokenType::LessEqual => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Boolean(l <= r))),
+            TokenType::Minus => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Number(l - r))),
+            TokenType::Slash => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Number(l / r))),
+            TokenType::Star => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Number(l * r))),
+            TokenType::Plus => cast_nums(&left, &right).and_then(|(l, r)| Ok(Value::Number(l + r)))
+                .or_else(|_| cast_strs(&left, &right).and_then(|(l, r)| Ok(Value::String(format!("{}{}", l, r)))))
+                .or(Err(cast_err("string or number"))),
+            TokenType::EqualEqual => Ok(Value::Boolean(left.is_equal(&right))),
+            TokenType::BangEqual => Ok(Value::Boolean(!left.is_equal(&right))),
+            _ => Ok(Value::Nil) // unreachable
+        }
+    }))
 }
 
 #[derive(Debug, Clone, PartialEq)]
